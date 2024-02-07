243ecaad085dcc544778a09e07d7c44848b92fe8
Shivanjali More <shivanjalimore035@gmail.com>
2024-02-07 10:59:55 +0530

    Create Longest_common_prefix_14.java

---
+++ LeetCode/String/Easy/Longest_common_prefix_14.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Longest_common_prefix_14 {
+    public String longestCommonPrefix(String[] strs) {
+
+StringBuilder result = new StringBuilder();
+
+// Sort the array
+Arrays.sort(strs);
+
+// Get the first and last strings
+char[] first = strs[0].toCharArray();
+char[] last = strs[strs.length - 1].toCharArray();
+
+// Start comparing
+for (int i = 0; i < first.length; i++) {
+  if (first[i] != last[i])
+    break;
+  result.append(first[i]);
+}
+
+return result.toString();
+}
+}
