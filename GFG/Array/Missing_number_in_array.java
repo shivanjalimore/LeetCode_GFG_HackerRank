e6f78d74b6d277c0e532539f36072f0d1a0c4a42
Shivanjali More <shivanjalimore035@gmail.com>
2024-02-07 10:59:38 +0530

    Create Missing_number_in_array.java

---
+++ GFG/Array/Missing_number_in_array.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Missing_number_in_array {
+     int missingNumber(int array[], int n) {
+        // Your Code Here
+       Arrays.sort(array);
+       for(int i=0;i<n-1;i++)
+       {
+           if(array[i] != i+1)
+           {
+               return i+1;
+           }
+       }
+       return n;
+    }
+}
